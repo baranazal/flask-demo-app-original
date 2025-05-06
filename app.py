8e6b76c835f65a534d927b7a2534fab347a5bb32
Bara <you@example.com>
2025-05-06 09:40:50 +0300

    edited

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
-from flask import Flask
-from flask import Flask, render_template
-from flask import Flask, render_template, redirect, url_for, request
+import socket
 import os
 import uuid
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    hostname = os.uname()[1]
+    hostname = socket.gethostname()
     randomid = uuid.uuid4()
     my_name = os.environ.get('MY_NAME', 'Default Name')
     my_role = os.environ.get('MY_ROLE', 'Default Role')
+    
     return (
-        f'Container Hostname: {hostname}\n'
-        f'UUID: {randomid}\n'
-        f'Name: {my_name}\n'
-        f'Role: {my_role}\n'
+        f'Container Hostname: {hostname}<br>'
+        f'UUID: {randomid}<br>'
+        f'Name: {my_name}<br>'
+        f'Role: {my_role}<br>'
     )
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -31,4 +31,3 @@ def login():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
-    
